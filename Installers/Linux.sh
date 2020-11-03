3c338f67809f6b43e0416d9d4e9771b8c45dc8d5
Ben Camarata <benmcamarata@gmail.com>
2020-11-03 07:56:34 -0800

    Update Linux.sh

--- Installers/Linux.sh
+++ Installers/Linux.sh
@@ -4,12 +4,15 @@ deps="libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 lib
 
 echo "Starboard 1.4.7 - install starting..."
 cd 
-mkdir Starboard
+mkdir ohsinternal
+mkdir starboardinternal
+
 # here is where we do are work. don't delete the folder!
-cd Starboard
+cd starboardinternal
 curl https://github.com/starboardbrowser/installersupport/blob/master/starboardstandard_1.4.7_amd64.deb?raw=true --output starboardstandard_1.4.7_amd64.deb
 curl https://github.com/starboardbrowser/installersupport/blob/master/starboardstandard_1.4.7_i386.deb?raw=true --output starboardstandard_1.4.7_i386.deb
 
+cd ohsinternal
 echo "Installer files have been downloaded."
 
 export DEBIAN_FRONTEND=noninteractive
@@ -20,4 +23,10 @@ echo "Dependencies have been downloaded."
 echo "Installing Starboard..."
 sudo dpkg -i starboardforamd.deb
 
+echo "Writing Logs & Finishing Up..."
+cd 
+cd ohsinternal
+touch InstallerLog.txt
+cat "Successfully found all deps and installed." >> InstallerLog.txt
+cd 
 echo "Starboard has been downloaded. Thank you."
